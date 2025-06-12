2eca49fd579e76678f9ed15d83a668b893c4d96c
Danilo25052 <josedanilobackups@gmail.com>
2025-06-12 16:58:22 -0300

    ....

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-function expandHideTopBar(element) {
-  console.log(element.parent);
-  console.log(typeof(element.parent));
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+
+  const btn = document.querySelector("#button-switch-theme");
+  if (document.body.classList.contains("dark-mode")) {
+    btn.textContent = "🌞";
+  } else {
+    btn.textContent = "🌗";
+  }
 }
